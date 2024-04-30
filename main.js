34747c2b94414798fa1ba2981cb3ed34464b5db5
Aaron Phung <user.name>
2024-04-30 16:22:31 -0700

    added MUGIC communication in main.js, renderer-accessible events in preload.js

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const url = require('url');
 const path = require('path');
+
+const osc = require('node-osc');
+
+const oscAvailableIPRange = "0.0.0.0";
+const addressPortMUGIC = 4000;
+
 
 function createMainWindow() {
     mainWindow = new BrowserWindow({
         title: 'MUGIC BOWING',
         width: 800,
         height: 600,
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: true,
+          preload: path.join(__dirname, 'preload.js')
+        }
     
     });
     const startUrl = url.format({
         pathname: path.join(__dirname, './app/build/index.html'),
         protocol: 'file',
     });
+
+
 
 
     mainWindow.loadURL(startUrl);
@@ -30,6 +43,63 @@ app.whenReady().then(() => {
       // dock icon is clicked and there are no other windows open.
       if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
+    // Start communication with MUGIC device
+    let isConnected = false;
+    function openAndListenToMugic(addressPortMUGIC, oscAvailableIPRange, retryInterval = 2000, maxRetries = 3) {
+      let retryCount = 0;
+      let isConnecting = false; // Track if a connection attempt is ongoing
+
+      function connect() {
+        if (isConnecting) return; // Don't start a new connection if one is ongoing
+        isConnecting = true;
+
+        const oscServer = new osc.Server(addressPortMUGIC, oscAvailableIPRange, () => {
+          console.log("listening")
+        });
+        console.log(oscServer)
+        oscServer.on('error', function (err) {
+          isConnecting = false;
+          console.log('error')
+          console.log(err);
+          mainWindow.webContents.send('mugic-error', err);
+          retryConnection(err);
+        });
+
+        oscServer.on('message', function (msg, rinfo) {
+          console.log("msg");
+          console.log(msg);
+          mainWindow.webContents.send('mugic-message', msg);
+          resetRetryCount();
+        });
+      }
+
+      function resetRetryCount() {
+        retryCount = 0;
+      }
+
+      function retryConnection(err) {
+        console.log("Unable to connect to IP address or Port: " + err);
+        if (retryCount < maxRetries) {
+          retryCount++;
+          console.log(`Retrying in ${retryInterval / 1000} seconds (Retry ${retryCount} of ${maxRetries})`);
+          setTimeout(() => {
+            connect(); // Attempt to connect again after the retry interval
+          }, retryInterval);
+        } else {
+          isConnecting = false;
+          console.log('Max retries reached. Could not establish a connection.');
+        }
+      }
+      try {
+      console.log("connecting")
+      connect(); // Initial connection attempt
+      isConnected = true;
+      }catch(err){
+        retryConnection(err)
+      }
+    }
+
+    openAndListenToMugic(4000, "0.0.0.0")
   })
   
   // Quit when all windows are closed, except on macOS. There, it's common
---
+++ preload.js
@@ -0,0 +1,24 @@
+/**
+ * The preload script runs before. It has access to web APIs
+ * as well as Electron's renderer process modules and some
+ * polyfilled Node.js functions.
+ *
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ */
+const { contextBridge, ipcRenderer } = require('electron')
+
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+
+})
+contextBridge.exposeInMainWorld('electronAPI', {
+  onMugicError: (callback) => ipcRenderer.on('mugic-error', (_event, value) => callback(value)),
+  onMugicMessage: (callback) => ipcRenderer.on('mugic-message', (_event, value) => callback(value))
+})
