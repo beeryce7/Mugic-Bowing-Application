ce25a8e4322d773a91550ff8fb5bdae6f96caa27
Michael de Vera <michael.devera0000@gmail.com>
2024-05-01 22:23:11 -0700

    fix bug where page wont load on desktop

--- app/src/App.js
+++ app/src/App.js
@@ -2,17 +2,17 @@ import Home from './pages/home/Home.js';
 import Settings from './pages/settings/Settings.js'
 import PlaySession from './pages/PlaySession/PlaySession.js';
 import './global.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index element={<Home/>}/>
         <Route path="settings" element={<Settings/>}/>
         <Route path="play" element={<PlaySession/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
 
   );
 }
