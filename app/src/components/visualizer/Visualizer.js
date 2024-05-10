1238169840454350a520411cd9773d5239c5a35d
Aaron Phung <user.name>
2024-05-09 17:46:43 -0700

    progress on Visualizer

--- app/src/components/visualizer/Visualizer.js
+++ app/src/components/visualizer/Visualizer.js
@@ -1,17 +1,37 @@
 import React, { useState, useRef } from "react";
-import { MugicTracker } from "../mugic_tracker/MugicTracker.js";
+import "./Visualizer.css"
+import MugicTracker from "../mugic_tracker/MugicTracker";
 import { Stage, Layer, Line, Circle } from "react-konva";
 
 const Visualizer = () => {
-    const width = 500
-    const height = 500
+    const width = 500;
+    const height = 500;
+    const minYaw = 0;
+    const maxYaw = 360;
+    const minRoll = 180;
+    const maxRoll = 180;
+
+
+    const[mugicData, setMugicData] = useState({
+        accel: [0,0,0],
+        euler: [0,0,0],
+        gyro: [0,0,0],
+        quatern: [0,0,0,0],
+        battery: 0,
+        seqNum: 0,
+    })
 
     return (
-        <Stage width={width} height={height}>
+        <div>
+            <MugicTracker mugicData={mugicData} setMugicData={setMugicData}/>
+            <Stage width={width} height={height}>
 
-          <Layer>
-            <Circle x={0} y={0} radius={50} fill="blue"/>
-          </Layer>
-        </Stage>
+                <Layer>
+                    <Circle x={((mugicData.euler[2] + minRoll) / (maxRoll + minRoll)) / width} y={((mugicData.euler[0] + minYaw) / (maxYaw + minYaw)) / height} radius={50} fill="blue"/>
+                </Layer>
+            </Stage>
+        </div>
     );
 }
+
+export default Visualizer;
