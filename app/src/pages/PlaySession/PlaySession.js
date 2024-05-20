814534977c7250752b0524aefe40865eab629f51
Bryce Green <brycepeter10@gmail.com>
2024-05-20 16:14:08 -0700

    Added a countdown function in PlaySession,js but I do not know how to implement

--- app/src/pages/PlaySession/PlaySession.js
+++ app/src/pages/PlaySession/PlaySession.js
@@ -9,19 +9,44 @@ const PlaySession = () => {
   return (
     <div>
     <Header/>
-    <div className="top-graph"><div className="key"><div class="color-block-teacher"></div>Teacher</div></div>
-    <div className="top-graph"><div className="key"><div class="color-block-student"></div>Student</div></div>
+    <div className="top-graph"><div className="key"><div class="color-block-teacher"></div><div className="change-font">Teacher</div></div></div>
+    <div className="top-graph"><div className="key"><div class="color-block-student"></div><div className="change-font">Student</div></div></div>
     <div className="be-on-right">00:00:00</div>
     <div className="new-graph">GRAPH</div>
 
     <SessionState/>
-    <h2>Stats</h2>
+    <div className="offset-right"><h2>Stats</h2>
     <li> Accuracy</li>
-    <li>Angle</li>
+    <li>Angle</li></div>
     {/* <MugicTracker/> */}
 
     </div> 
   );
 };
+
+// Countdown function
+/*function countdown() {
+  // Get the countdown element
+  const countdownElement = document.getElementById('countdown');
+
+  // Set the initial countdown value
+  let count = 3;
+
+  // Update the countdown every second
+  const timer = setInterval(function() {
+      // Display the current countdown value
+      countdownElement.innerText = count;
+
+      // Decrement the count
+      count--;
+
+      // If the count reaches 0, stop the countdown
+      if (count < 0) {
+          clearInterval(timer);
+          countdownElement.innerText = 'Countdown Complete!';
+      }
+  }, 1000);
+}*/
+
 
 export default PlaySession;
